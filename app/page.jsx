fd66870d15c2585ce6117b7d26673ac576eb8d6e
Manan Upadhyay <manantheassassin@gmail.com>
2025-03-19 16:31:11 +0530

    Created a recent properties section

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import Infoboxes from "@/components/Infoboxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Infoboxes />
+      <HomeProperties />
     </>
   );
 };
